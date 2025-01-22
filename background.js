c3b16d6cf9b0d438a68e91093e3d6bcf436f584a
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2025-01-22 17:31:17 -0500

    Update background.js

--- background.js
+++ background.js
@@ -9,22 +9,33 @@ const redirects = {
   "www.tiktok.com": "https://pixelfed.org",
   "facebook.com": "https://mastodon.social",
   "www.facebook.com": "https://mastodon.social",
-  "youtube.com": "https://yewtu.be",
-  "www.youtube.com": "https://yewtu.be",
+  "youtube.com": "https://invidious.nerdvpn.de",
+  "www.youtube.com": "https://invidious.nerdvpn.de",
   "fandom.com": "https://antifandom.com",
   "www.fandom.com": "https://antifandom.com"
 };
 
+const pathRetainingRedirects = [
+  "youtube.com",
+  "www.youtube.com"
+];
+
 const blockedPages = {
-  "meta.com": "/blocked.html",
-  "www.meta.com": "/blocked.html"
+  "example.com": "/blocked.html",
+  "www.example.com": "/blocked.html"
 };
 
 browser.webRequest.onBeforeRequest.addListener(
   function(details) {
     const url = new URL(details.url);
     if (redirects[url.hostname]) {
-      return { redirectUrl: redirects[url.hostname] };
+      const redirectBase = redirects[url.hostname];
+      if (pathRetainingRedirects.includes(url.hostname)) {
+        const redirectUrl = redirectBase + url.pathname + url.search + url.hash;
+        return { redirectUrl: redirectUrl };
+      } else {
+        return { redirectUrl: redirectBase };
+      }
     } else if (blockedPages[url.hostname]) {
       return { redirectUrl: browser.runtime.getURL(blockedPages[url.hostname]) };
     }
